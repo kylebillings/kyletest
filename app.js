14b313d34ff2b348f6b968ccb0167ba3fffbaaec
kyle billings <the.kyle.billings@gmail.com>
2014-10-21 15:55:49 -0700

    redesign... again

--- app.js
+++ app.js
@@ -10,3 +10,17 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+$(document).ready(function(){
+	var d = new Date();
+	var n = d.getHours();
+	if (n > 21 || n < 6)
+	  // If time is after 9PM or before 6AM, apply night theme to ‘body’
+	  document.body.className = "night";
+	else if (n > 17 && n < 21)
+	  // If time is between 5PM – 9PM sunset theme to ‘body’
+	  document.body.className = "sunset";
+	else
+	  // Else use ‘day’ theme
+	  document.body.className = "day";
+});
