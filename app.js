3ee9fc7c85b60503e68553b91f99b9ee4dd6b0c0
kyle billings <the.kyle.billings@gmail.com>
2014-10-21 23:22:53 -0700

    redesign... again

--- app.js
+++ app.js
@@ -14,13 +14,24 @@ config(['$routeProvider', function($routeProvider) {
 $(document).ready(function(){
 	var d = new Date();
 	var n = d.getHours();
-	if (n > 21 || n < 6)
+	if (n > 19 || n < 6)
 	  // If time is after 9PM or before 6AM, apply night theme to ‘body’
 	  document.body.className = "night";
-	else if (n > 16 && n < 21)
+	else if (n > 16 && n < 20)
 	  // If time is between 5PM – 9PM sunset theme to ‘body’
 	  document.body.className = "sunset";
 	else
 	  // Else use ‘day’ theme
 	  document.body.className = "day";
+});
+
+$('.heart').on('click', function() {
+  
+  var $this = $(this);
+  
+  $this.addClass('beating');
+  
+  //setTimeout(function() {
+  //  $this.removeClass('beating');
+  //}, 500);
 });
